ee6e8da9a48ce002b8283c1912d56313004fb578
Vincent Guo <vincentguo04@gmail.com>
2020-05-21 12:59:04 -0400

    Added addChips function to Board class
    Signed-off-by: Vincent Guo <vincentguo04@gmail.com>

--- src/Board.java
+++ src/Board.java
@@ -2,7 +2,7 @@ public class Board {
     final int H = 6, W = 7; //board dimensions
     int[][] board = new int[H][W];
 
-    boolean checkWin(int x, int y, int player) {
+    public boolean checkWin(int x, int y, int player) {
         int cL = 0, cU = 0, cRU = 0, cRD = 0;
         boolean lF = false, rF = false, uF = false, dF = false, ruF = false, ldF = false, luF = false, rdF = false;
         for (int i = 0; i <= H; i++) {
@@ -40,5 +40,10 @@ public class Board {
             }
         }
         return cL >= 4 || cU >= 4 || cRD >= 4 || cRU >= 4;
+    }
+
+    public boolean addChip(int x, int y, int player) {
+        board[x][y] = player;
+        return checkWin(x, y, player);
     }
 }
