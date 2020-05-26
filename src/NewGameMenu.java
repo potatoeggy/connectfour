956d6da5a6369a5e0d3169ecab31b5bdb5e91c5e
Daniel Chen <25178974+potatoeggy@users.noreply.github.com>
2020-05-26 11:59:47 -0400

    menus: split panels off in classes for readability, event handling handled by main

--- src/NewGameMenu.java
+++ src/NewGameMenu.java
@@ -1,22 +1,29 @@
 // Daniel Chen
-// 13 May 2020
-// main window for user interaction
+// 26 May 2020
+// New game options selection
 
-import java.awt.*;
+import java.awt.Component;
+import java.awt.GridLayout;
 import java.awt.event.*;
-import javax.swing.*;
 
-public class MainMenu extends JFrame implements ActionListener {
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class NewGameMenu extends JPanel {
     private JPanel verticalBox, playerBox; // panels used to hold things
     private JLabel title; // "Connect 4" header
     private JLabel[] nameLabels, playerLabels; // instructions for user interaction
     private JComboBox<String>[] playerSelect; // select player
     private JTextField[] nameSelect; // select name
-    private JButton[] buttons; // continue or quit
+    JButton[] buttons; // continue or quit
 
-    public MainMenu() {
-        // create all objects
-        verticalBox = new JPanel();
+    public NewGameMenu(ActionListener eventHandler) {
+        // create all objects for frame menu
         playerBox = new JPanel();
         title = new JLabel("Connect 4");
         nameLabels = new JLabel[] {new JLabel("Name: "), new JLabel("Name: ")};
@@ -25,17 +32,16 @@ public class MainMenu extends JFrame implements ActionListener {
         nameSelect = new JTextField[] {new JTextField("Player 1"), new JTextField("Player 2")};
         buttons = new JButton[] {new JButton("Start game"), new JButton("Options"), new JButton("Exit")};
 
-        verticalBox.setLayout(new BoxLayout(verticalBox, BoxLayout.PAGE_AXIS));
-        verticalBox.add(title);
-        verticalBox.add(Box.createVerticalStrut(10)); // add spacing
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        add(title);
+        add(Box.createVerticalStrut(10)); // add spacing
         title.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-        verticalBox.add(playerBox);
-        verticalBox.add(Box.createVerticalStrut(10));
+        add(playerBox);
         for (JButton butt : buttons) { // add all buttons and make them do something
             butt.setAlignmentX(JButton.CENTER_ALIGNMENT);
-            butt.addActionListener(this);
-            verticalBox.add(butt);
-            verticalBox.add(Box.createVerticalStrut(10));
+            butt.addActionListener(eventHandler);
+            add(Box.createVerticalStrut(10));
+            add(butt);
         }
 
         playerBox.setLayout(new GridLayout(2, 4, 5, 5));
@@ -50,30 +56,5 @@ public class MainMenu extends JFrame implements ActionListener {
                 cb.addItem(s);
             }
         }
-
-        add(verticalBox);
-        setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
-        getRootPane().setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20)); // make margins so it's nicer to use
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setTitle("Connect 4");
-        setSize(800, 300);
-    }
-
-    public void actionPerformed(ActionEvent event) {
-        Object e = event.getSource();
-        if (e.equals(buttons[0])) {
-
-        } else if (e.equals(buttons[1])) {
-
-        } else if (e.equals(buttons[2])) {
-            dispose(); // quit
-            // TODO: make sure to implement every other panel too
-            // TODO: make sure to dispose frames (only modal dialogs should be frames)
-        }
-    }
-
-    public static void main(String[] args) {
-        MainMenu win = new MainMenu();
-        win.setVisible(true);
     }
 }
