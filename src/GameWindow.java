9e5cf33ad611fe701de70f98715e40354d6c13b4
Daniel Chen <25178974+potatoeggy@users.noreply.github.com>
2020-05-29 18:03:41 -0400

    gamewindow: documentation

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -7,14 +7,14 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class GameWindow extends JPanel implements ActionListener {
-	private JPanel header, body;
-	private JPanel[] headerJustification;
-	JButton[] headerButtons;
-	JButton optionsButton;
-	private JLabel gameStatus, moveTimer;
-	private JButton[][] buttonGrid = new JButton[7][7];
-	private ImageIcon arrow;
-	private MainWindow eventHandler;
+	private JPanel header, body; // two panels for main game and status
+	private JPanel[] headerJustification; // align things left, centre, and right
+	JButton[] headerButtons; // new game, save and quit
+	JButton optionsButton; // options
+	private JLabel gameStatus, moveTimer; // status and timer, if enabled
+	private JButton[][] buttonGrid = new JButton[7][7]; // connect 4 grid
+	private ImageIcon arrow; // shows where the square is going to fall
+	private MainWindow eventHandler; // global event handler and settings manager
 
 	public GameWindow(MainWindow eventHandler, Board board) {
 		this.eventHandler = eventHandler;
@@ -28,40 +28,40 @@ public class GameWindow extends JPanel implements ActionListener {
 		arrow = new ImageIcon("resources/arrow.png");
 
 		for (JButton butt : headerButtons) {
-			headerJustification[0].add(butt);
+			headerJustification[0].add(butt); // justify left
 			butt.addActionListener(eventHandler);
 		}
 
-		headerJustification[1].add(gameStatus);
-		headerJustification[2].add(optionsButton);
+		headerJustification[1].add(gameStatus); // justify centre
+		headerJustification[2].add(optionsButton); // justify right
 		optionsButton.addActionListener(eventHandler);
-		headerJustification[2].add(moveTimer);
+		headerJustification[2].add(moveTimer); // justify right
 
-		for (JPanel pan : headerJustification) {
+		for (JPanel pan : headerJustification) { // fill in the header
 			header.add(pan);
 		}
-		headerJustification[0].setLayout(new FlowLayout(FlowLayout.LEFT));
+		headerJustification[0].setLayout(new FlowLayout(FlowLayout.LEFT)); // the layout justifies things
 		headerJustification[1].setLayout(new FlowLayout(FlowLayout.CENTER));
 		headerJustification[2].setLayout(new FlowLayout(FlowLayout.RIGHT));
 
-		for (int i = 0; i < buttonGrid.length; i++) {
+		for (int i = 0; i < buttonGrid.length; i++) { // fill in the button arrays
 			for (int j = 0; j < buttonGrid[i].length; j++) {
-				buttonGrid[i][j] = new JButton("<html><br><br><br><br><br><br></html>");
+				buttonGrid[i][j] = new JButton("<html><br><br><br><br><br><br></html>"); // make multi-line buttons
 				buttonGrid[i][j].addActionListener(this);
-				buttonGrid[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
-				buttonGrid[i][j].putClientProperty("x", new Integer(i));
+				buttonGrid[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK)); // create a grid
+				buttonGrid[i][j].putClientProperty("x", new Integer(i)); // store coordinates
 				buttonGrid[i][j].putClientProperty("y", new Integer(j));
-				buttonGrid[i][j].setBackground(Color.WHITE);
-				buttonGrid[i][j].setFocusPainted(false);
+				buttonGrid[i][j].setBackground(Color.WHITE); // make pretty
+				buttonGrid[i][j].setFocusPainted(false); // do not have ugly lines
 				buttonGrid[i][j].addMouseListener(new MouseAdapter() {
-					public void mouseEntered(MouseEvent event) {
+					public void mouseEntered(MouseEvent event) { // highlight column when mouse goes over things
 						int y = (Integer) (((JButton) event.getSource()).getClientProperty("y"));
 						buttonGrid[0][y].setIcon(arrow);
 						revalidate();
 						repaint();
 					}
 
-					public void mouseExited(MouseEvent event) {
+					public void mouseExited(MouseEvent event) { // get rid of highlight when mouse leaves
 						int y = (Integer) (((JButton) event.getSource()).getClientProperty("y"));
 						buttonGrid[0][y].setIcon(null);
 					}
@@ -71,7 +71,7 @@ public class GameWindow extends JPanel implements ActionListener {
 		}
 
 		for (int i = 0; i < buttonGrid[0].length; i++) {
-			buttonGrid[0][i].setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.WHITE));
+			buttonGrid[0][i].setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.WHITE)); // make top open-ended
 		}
 
 		add(header);
