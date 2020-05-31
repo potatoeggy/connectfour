276804e3853542ffb3b7d59696b327543076856b
Vincent Guo <vincentguo04@gmail.com>
2020-05-31 14:18:09 -0400

    Removed unnecessary boxing on Integer
    Signed-off-by: Vincent Guo <vincentguo04@gmail.com>

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -2,26 +2,31 @@
 // 28 May 2020
 // The bulk of the annoying stuff to do
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GameWindow extends JPanel implements ActionListener {
-	private JPanel header, body; // two panels for main game and status
-	private JPanel[] headerJustification; // align things left, centre, and right
+	private final JPanel header;
+	private final JPanel body; // two panels for main game and status
+	private final JPanel[] headerJustification; // align things left, centre, and right
 	JButton[] headerButtons; // new game, save and quit
 	JButton optionsButton; // options
-	private JLabel gameStatus, moveTimer; // status and timer, if enabled
-	private JButton[][] buttonGrid = new JButton[7][7]; // connect 4 grid
+	private final JLabel gameStatus;
+	private final JLabel moveTimer; // status and timer, if enabled
+	private final JButton[][] buttonGrid = new JButton[7][7]; // connect 4 grid
 	private ImageIcon arrow, redPiece, yellowPiece; // graphics
-	private MainWindow eventHandler; // global event handler and settings manager
+	private final MainWindow eventHandler; // global event handler and settings manager
 
 	public GameWindow(MainWindow eventHandler, Board board) {
 		this.eventHandler = eventHandler;
 		header = new JPanel();
-		headerJustification = new JPanel[] {new JPanel(), new JPanel(), new JPanel()};
+		headerJustification = new JPanel[]{new JPanel(), new JPanel(), new JPanel()};
 		body = new JPanel();
-		headerButtons = new JButton[] {new JButton("Save & quit"), new JButton("New game")};
+		headerButtons = new JButton[]{new JButton("Save & quit"), new JButton("New game")};
 		optionsButton = new JButton("Options");
 		gameStatus = new JLabel("temporary header");
 		moveTimer = new JLabel("move timer");
@@ -55,8 +60,8 @@ public class GameWindow extends JPanel implements ActionListener {
 				buttonGrid[i][j] = new JButton("<html><br><br><br><br><br><br></html>"); // make multi-line buttons
 				buttonGrid[i][j].addActionListener(this);
 				buttonGrid[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK)); // create a grid
-				buttonGrid[i][j].putClientProperty("x", new Integer(i)); // store coordinates
-				buttonGrid[i][j].putClientProperty("y", new Integer(j));
+				buttonGrid[i][j].putClientProperty("x", i); // store coordinates
+				buttonGrid[i][j].putClientProperty("y", j);
 				buttonGrid[i][j].setBackground(Color.WHITE); // make pretty
 				buttonGrid[i][j].setFocusPainted(false); // do not have ugly lines
 				buttonGrid[i][j].addMouseListener(new MouseAdapter() {
