82433843531afcbb973332080f345689bedee62d
Daniel Chen <25178974+potatoeggy@users.noreply.github.com>
2020-06-09 11:41:44 -0400

    fix wrong parameter for user notification of verbosity

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -152,7 +152,7 @@ public class MainWindow extends JFrame implements ActionListener {
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		if (args.length > 0 && args[0].equals("verbose")) System.out.println("[DEBUG] Board history printing enabled.");
+		if (args.length > 0 && args[0].equals("--verbose")) System.out.println("[DEBUG] Board history printing enabled.");
 		for (String s : new String[] {
 			"Label",
 			"Button",
